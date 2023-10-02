fc9992b321ebfcc5c16debbc6c9aeda23b6109c8
Evans <manotievance@gmail.com>
2023-10-02 11:48:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record){
+    const res = record.find(element => element.result === 'W')
+    return !!res ? res.year : undefined
+}
+
+
+
+
+console.log(superbowlWin(record))
+
